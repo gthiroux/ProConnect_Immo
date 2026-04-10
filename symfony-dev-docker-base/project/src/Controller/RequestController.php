813c63d3c446d754810ed55cd356247ed68f2abd
Mateo Ribeiro <mateo.ribeiro314@gmail.com>
2026-04-10 13:23:28 +0200

    request controller

--- symfony-dev-docker-base/project/src/Controller/RequestController.php
+++ symfony-dev-docker-base/project/src/Controller/RequestController.php
@@ -2,6 +2,40 @@
 
 namespace App\Controller;
 
+use App\Repository\RequestRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class RequestController extends AbstractController
+{
+	#[Route('/accueil', name: 'app_accueil')]
+	public function index(RequestRepository $repo): Response
+	{
+		foreach ($repo->selectNullUUID() as $r) {
+			$r->setUUID($r->getEmail());
+			$repo->updateNullUUID($r->getUUID(), $r->getEmail());
+		}
+		$uniRequests = $repo->selectGroupByUUID();
+		return $this->render('request/index.html.twig', [
+			'controller_name' => 'RequestController',
+			'requests' => $uniRequests,
+		]);
+	}
+
+	#[Route('/accueil/{UUID}', name: 'app_folder_details')]
+	public function detailsFolder(RequestRepository $repo, string $UUID): Response
+	{
+		$requests = $repo->findByUUID($UUID);
+		return $this->render('request/detailsFolder.html.twig', [
+			'controller_name' => 'RequestController',
+			'requests' => $requests,
+		]);
+	}
+}
+
+namespace App\Controller;
+
 use App\Entity\Request;
 use App\Form\RequestType;
 use App\Repository\RequestRepository;
@@ -13,68 +47,68 @@ use Symfony\Component\Routing\Attribute\Route;
 #[Route('/accueil')]
 final class RequestController extends AbstractController
 {
-    #[Route(name: 'app_request_index', methods: ['GET'])]
-    public function index(RequestRepository $requestRepository): Response
-    {
-        return $this->render('request/index.html.twig', [
-            'requests' => $requestRepository->findAll(),
-        ]);
-    }
+	#[Route(name: 'app_request_index', methods: ['GET'])]
+	public function index(RequestRepository $requestRepository): Response
+	{
+		return $this->render('request/index.html.twig', [
+			'requests' => $requestRepository->findAll(),
+		]);
+	}
 
-    #[Route('/new', name: 'app_request_new', methods: ['GET', 'POST'])]
-    public function new(Request $request, EntityManagerInterface $entityManager): Response
-    {
-        $request = new Request();
-        $form = $this->createForm(RequestType::class, $request);
-        $form->handleRequest($request);
+	#[Route('/new', name: 'app_request_new', methods: ['GET', 'POST'])]
+	public function new(Request $request, EntityManagerInterface $entityManager): Response
+	{
+		$request = new Request();
+		$form = $this->createForm(RequestType::class, $request);
+		$form->handleRequest($request);
 
-        if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager->persist($request);
-            $entityManager->flush();
+		if ($form->isSubmitted() && $form->isValid()) {
+			$entityManager->persist($request);
+			$entityManager->flush();
 
-            return $this->redirectToRoute('app_request_index', [], Response::HTTP_SEE_OTHER);
-        }
+			return $this->redirectToRoute('app_request_index', [], Response::HTTP_SEE_OTHER);
+		}
 
-        return $this->render('request/new.html.twig', [
-            'request' => $request,
-            'form' => $form,
-        ]);
-    }
+		return $this->render('request/new.html.twig', [
+			'request' => $request,
+			'form' => $form,
+		]);
+	}
 
-    #[Route('/{id}', name: 'app_request_show', methods: ['GET'])]
-    public function show(Request $request): Response
-    {
-        return $this->render('request/show.html.twig', [
-            'request' => $request,
-        ]);
-    }
+	#[Route('/{id}', name: 'app_request_show', methods: ['GET'])]
+	public function show(Request $request): Response
+	{
+		return $this->render('request/show.html.twig', [
+			'request' => $request,
+		]);
+	}
 
-    #[Route('/{uuid}/edit', name: 'app_request_edit', methods: ['GET', 'POST'])]
-    public function edit(Request $request, EntityManagerInterface $entityManager): Response
-    {
-        $form = $this->createForm(RequestType::class, $request);
-        $form->handleRequest($request);
+	#[Route('/{uuid}/edit', name: 'app_request_edit', methods: ['GET', 'POST'])]
+	public function edit(Request $request, EntityManagerInterface $entityManager): Response
+	{
+		$form = $this->createForm(RequestType::class, $request);
+		$form->handleRequest($request);
 
-        if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager->flush();
+		if ($form->isSubmitted() && $form->isValid()) {
+			$entityManager->flush();
 
-            return $this->redirectToRoute('app_request_index', [], Response::HTTP_SEE_OTHER);
-        }
+			return $this->redirectToRoute('app_request_index', [], Response::HTTP_SEE_OTHER);
+		}
 
-        return $this->render('request/edit.html.twig', [
-            'request' => $request,
-            'form' => $form,
-        ]);
-    }
+		return $this->render('request/edit.html.twig', [
+			'request' => $request,
+			'form' => $form,
+		]);
+	}
 
-    #[Route('/{uuid}', name: 'app_request_delete', methods: ['POST'])]
-    public function delete(Request $request, EntityManagerInterface $entityManager): Response
-    {
-        if ($this->isCsrfTokenValid('delete'.$request->getId(), $request->getPayload()->getString('_token'))) {
-            $entityManager->remove($request);
-            $entityManager->flush();
-        }
+	#[Route('/{uuid}', name: 'app_request_delete', methods: ['POST'])]
+	public function delete(Request $request, EntityManagerInterface $entityManager): Response
+	{
+		if ($this->isCsrfTokenValid('delete' . $request->getId(), $request->getPayload()->getString('_token'))) {
+			$entityManager->remove($request);
+			$entityManager->flush();
+		}
 
-        return $this->redirectToRoute('app_request_index', [], Response::HTTP_SEE_OTHER);
-    }
+		return $this->redirectToRoute('app_request_index', [], Response::HTTP_SEE_OTHER);
+	}
 }
--- symfony-dev-docker-base/project/src/Repository/RequestRepository.php
+++ symfony-dev-docker-base/project/src/Repository/RequestRepository.php
@@ -11,33 +11,64 @@ use Doctrine\Persistence\ManagerRegistry;
  */
 class RequestRepository extends ServiceEntityRepository
 {
-    public function __construct(ManagerRegistry $registry)
-    {
-        parent::__construct($registry, Request::class);
-    }
+	public function __construct(ManagerRegistry $registry)
+	{
+		parent::__construct($registry, Request::class);
+	}
 
-    //    /**
-    //     * @return Request[] Returns an array of Request objects
-    //     */
-    //    public function findByExampleField($value): array
-    //    {
-    //        return $this->createQueryBuilder('r')
-    //            ->andWhere('r.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->orderBy('r.id', 'ASC')
-    //            ->setMaxResults(10)
-    //            ->getQuery()
-    //            ->getResult()
-    //        ;
-    //    }
+	/**
+	 * @return Request[] Returns an array of Request objects
+	 */
+	public function findByUUID($value): array
+	{
+		return $this->createQueryBuilder('r')
+			->andWhere('r.UUID = :val')
+			->setParameter('val', $value)
+			->orderBy('r.id', 'ASC')
+			->getQuery()
+			->getResult()
+		;
+	}
 
-    //    public function findOneBySomeField($value): ?Request
-    //    {
-    //        return $this->createQueryBuilder('r')
-    //            ->andWhere('r.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->getQuery()
-    //            ->getOneOrNullResult()
-    //        ;
-    //    }
+	public function selectNullUUID(): array
+	{
+		return $this->createQueryBuilder('r')
+			->andWhere('r.UUID IS NULL')
+			->groupBy('r.email')
+			->getQuery()
+			->getResult()
+		;
+	}
+
+	public function updateNullUUID($UUID, $email): int
+	{
+		return $this->createQueryBuilder('r')
+			->update('App\\Entity\\Request', 'r')
+			->set('r.UUID', ':UUID')
+			->setParameter('UUID', $UUID)
+			->andWhere('r.email = :email')
+			->setParameter('email', $email)
+			->getQuery()
+			->execute()
+		;
+	}
+
+	public function selectGroupByUUID(): array
+	{
+		return $this->createQueryBuilder('r')
+			->groupBy('r.UUID')
+			->getQuery()
+			->getResult()
+		;
+	}
+
+	//    public function findOneBySomeField($value): ?Request
+	//    {
+	//        return $this->createQueryBuilder('r')
+	//            ->andWhere('r.exampleField = :val')
+	//            ->setParameter('val', $value)
+	//            ->getQuery()
+	//            ->getOneOrNullResult()
+	//        ;
+	//    }
 }
